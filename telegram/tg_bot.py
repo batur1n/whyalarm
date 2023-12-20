23598a728b1bc56879a10996a2ad5fcc3097341f
batur1n <misha.baturin@gmail.com>
2023-12-20 17:38:50 +0200

    fix codestyle issue

--- telegram/tg_bot.py
+++ telegram/tg_bot.py
@@ -1,19 +1,25 @@
+"""Module for working with telegram bot"""
+import sys
 import requests
 from my_secrets import TELEGRAM_BOT_TOKEN, CHAT_ID
 
 
 class TelegramBot:
+    """Class which contains telegram bot methods"""
 
     def __init__(self):
         self.token = TELEGRAM_BOT_TOKEN
-        self.bot_url = "https://api.telegram.org/bot{}/".format(self.token)
+        self.bot_url = f"https://api.telegram.org/bot{self.token}/"
         self.chat_id = CHAT_ID
 
     def send_message(self, message):
-        requests.get(self.bot_url + "sendMessage?text={}&chat_id={}".format(message, self.chat_id))
+        """Simple method to send any string message"""
+        requests.get(self.bot_url+f"sendMessage?text={message}&chat_id={self.chat_id}", timeout=10)
 
     def send_dict_as_message(self, dictionary):
+        """Method to send dict in a pretty printed format"""
         text = 'Останні повідомлення з телеграм каналів: '
         for key, value in dictionary.items():
             text = text + f"\n{key}: {value}\n"
         self.send_message(text)
+        sys.stdout.write("\nAlert reasons posted on telegram channel!")
